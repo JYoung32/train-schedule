fcdd087c6ac7aa3b9fb5a50558134dc7db626694
Joseph Young <joseph.young32@yahoo.com>
2020-02-19 19:13:16 -0500

    refactored into es6 syntax i.e. const/let, fat arrow function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 //firebase key
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyCD7GgljYGjeAvQ-Ml4buwRLmKofvHZZ00",
     authDomain: "homework-trainschedule-fd876.firebaseapp.com",
     databaseURL: "https://homework-trainschedule-fd876.firebaseio.com",
@@ -8,22 +8,19 @@ var firebaseConfig = {
     messagingSenderId: "18268162344",
     appId: "1:18268162344:web:f9af0205286c0891e7681d",
     measurementId: "G-NG7YG41XH0"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 //create a variable to reference the database
-var database = firebase.database();
+const database = firebase.database();
 
-
-//on click function for the submit button
-$(".btn-submit").on("click", function(event){
+buttonSubmit = (event) => {
     event.preventDefault();
 
-    var name = $("#trainName").val().trim();
-    var destination = $("#destination").val().trim();
-    var startTime = $("#startTime").val().trim();
-    var frequency = $("#frequency").val().trim();
+    const name = $("#trainName").val().trim();
+    const destination = $("#destination").val().trim();
+    const startTime = $("#startTime").val().trim();
+    const frequency = $("#frequency").val().trim();
 
     database.ref().push({
         name : name,
@@ -35,40 +32,39 @@ $(".btn-submit").on("click", function(event){
     $("#trainName, #destination, #startTime, #frequency").val("");
     //don't refresh the page
     return false;
-
-    
-});
+};
 
 //firebase watcher and initial loader.
-database.ref().on("child_added", function(childSnapshot){
+database.ref().on("child_added", (childSnapshot) => {
     //set childSnapshot to variables
-    var childName = childSnapshot.val().name;
-    var childDestination = childSnapshot.val().destination;
-    var childStartTime = childSnapshot.val().startTime;
-    var childFrequency = childSnapshot.val().frequency;
-
-    console.log(childStartTime);
+    const childName = childSnapshot.val().name;
+    const childDestination = childSnapshot.val().destination;
+    const childStartTime = childSnapshot.val().startTime;
+    const childFrequency = childSnapshot.val().frequency;
 
     // //create a moment object
-    var minAway;
+    let minAway;
     //change the year so the first train comes before now
-    var firstNewTrain = moment(childStartTime, "hh:mm").subtract(1, "years");
+    let firstNewTrain = moment(childStartTime, "hh:mm").subtract(1, "years");
     //difference between the current and first train
-    var diffTime = moment().diff(moment(firstNewTrain), "minutes");
-    var remainder = diffTime % childFrequency;
+    let diffTime = moment().diff(moment(firstNewTrain), "minutes");
+    let remainder = diffTime % childFrequency;
     //minutes until next train
     minAway = childFrequency - remainder;
     //next train time 
-    var nextTrain = moment().add(minAway, "minutes");
+    let nextTrain = moment().add(minAway, "minutes");
     nextTrain = moment(nextTrain).format("hh:mm");
 
-    var newRow = `<tr>
+    const newRow = `<tr>
                     <td>${childName}</td>
                     <td>${childDestination}</td>
                     <td>${childFrequency}</td>
                     <td>${nextTrain}</td>
                     <td>${minAway} mins</td>
-    </tr>`
+    </tr>`;
     //append content to the display table
     $("tbody").append(newRow);
 });
+
+//on click function for the submit button
+$(".btn-submit").on("click", (event) => { buttonSubmit(event) });
